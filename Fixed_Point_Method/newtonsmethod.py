8fb6c714586090345fc680be1494ca1890952fef
smmarche <105466339+smmarche@users.noreply.github.com>
2023-10-13 20:49:38 -0500

    Update newtonsmethod.py

--- Fixed_Point_Method/newtonsmethod.py
+++ Fixed_Point_Method/newtonsmethod.py
@@ -29,3 +29,28 @@ tolerance = 1e-6       # Tolerance for convergence
 root, iterations = newton(f, fp, x0, tolerance)
 print(f"Approximate root: {root:.5f}")
 print(f"Iteration count: {iterations}")
+
+# function f(x)
+f = lambda x: x - 2 * math.cosh(x / 4)
+
+# function derivative f'(x)
+fp = lambda x: 1 - 0.5 * math.sinh(x / 4)
+
+
+tolerance = 1e-13
+
+# first root guess
+x0_1 = 2.0  # You can choose an appropriate initial guess
+
+# Find the first root
+root1, iterations1 = newton(f, fp, x0_1, tolerance)
+print(f"Approximate root 1: {root1:.13f}")
+print(f"Iterations for root 1: {iterations1}")
+
+# second root guess
+x0_2 = 8.0  # You can choose an appropriate initial guess
+
+# Find the second root
+root2, iterations2 = newton(f, fp, x0_2, tolerance)
+print(f"Approximate root 2: {root2:.13f}")
+print(f"Iterations for root 2: {iterations2}")
